b4a0a8b96a7754f3d306e7dba90b3c3483b35a72
Steve Harris <steve.harris@garlik.com>
2010-06-12 13:33:10 +0100

    First add

---
+++ sparql-xmpp-server.pl
@@ -0,0 +1,176 @@
+#!/usr/bin/perl
+
+use Net::XMPP qw( Client );
+$Con = new Net::XMPP::Client();
+
+if (@ARGV != 2) {
+	die "Usage: $0 <jabber-address> <password>\n";
+}
+
+my $CID = shift;
+my $password = shift;
+
+local $KB = $ENV{'USER'}."foaf";
+local $OPT = "-f json";
+
+my($user, $host) = $CID =~ /(.*)@(.*)/;
+$status = $Con->Connect(hostname => $host);
+my @ret = $Con->AuthSend(
+	username => $user,
+	password => $password,
+	resource => $user
+);
+
+if ($ret[0] ne "ok") {
+	die "Failed to authenticate to server: $ret[1]";
+}
+
+# these are really low level
+$Con->SetCallBacks(
+	send=>\&sendCallBack,
+	receive=>\&receiveCallBack,
+	message=>\&messageCallBack,
+	iq=>\&handleTheIQTag
+);
+
+# higher level callbacks
+$Con->SetXPathCallBacks(
+	#"/message[\@type='chat']"=>\&messageChatCB,
+	"/message[\@type='chat']"=>\&otherMessageChatCB,
+);
+
+$Con->AddNamespace(
+	ns =>"http://www.w3.org/2005/09/xmpp-sparql-binding",
+	tag =>"query",
+	xpath => { SPARQL => { path => "text()" } }
+);
+
+my $Pres = new Net::XMPP::Presence();
+
+$Con->RosterGet();
+$Con->PresenceSend();
+
+#$Pres->SetPresence(from=>"$CID", type=>"available", status=>"online");
+#$ret = $Con->Send($Pres);
+
+$ret = $Con->MessageSend(
+	to => "theno23\@jabber.org",
+	subject => "server up",
+	body => "SPARQL server ".`uname -a`." awake",
+	thread => "inform",
+	priority => 10
+);
+
+my $quit = 0;
+
+while (!$quit) {
+	$status = $Con->Process(1);
+	if (!defined $status) {
+		$error = $Con->GetErrorCode();
+		die "Fatal error: $error";
+	}
+}
+
+$Con->Disconnect();
+
+sub otherMessageChatCB {
+	my($i, $m) = @_;
+
+	my $msg = $m->GetBody();
+
+	print("Chat body: ".$msg."\n");
+	if ($msg =~ /QUIT/) {
+		$quit = 1;
+	} elsif ($msg =~ /(SELECT|CONSTRUCT)/) {
+		my $to = $m->GetFrom();
+		my $from = $m->GetTo();
+		my $id = $m->GetID();
+		my $query = $msg;
+
+		sendtextres($from, $to, $id, $query);
+
+#		my $res = `4s-query $OPT '$KB' '$query'`;
+#		$res =~ s/\s+/ /g;
+#
+#		$ret = $Con->MessageSend(
+#			to => $to,
+#			subject => "Results",
+#			body => $res,
+#			thread => "results",
+#			priority => 10
+#		);
+	}
+}
+
+sub messageChatCB {
+	my($i, $m) = @_;
+
+	print("Body: ".$m->GetBody()."\n");
+}
+
+sub sendCallBack {
+	my ($id, $msg) = @_;
+
+	print "send: $msg\n";
+}
+
+sub receiveCallBack  {
+	my ($id, $msg) = @_;
+
+	print $msg."\n";
+}
+
+sub messageCallBack  {
+	print "mesg: ".join(",", @_)."\n";
+}
+
+sub handleTheIQTag  {
+	my($id, $iq) = @_;
+
+	my $to = $iq->GetFrom();
+	my $from = $iq->GetTo();
+	my $id = $iq->GetID();
+print("GOT IQ\n");
+return;
+	my $query = $iq->GetQuery()->GetSPARQL();
+	my $namespace = $iq->GetQueryXMLNS();
+
+	print "Got $namespace query\n";
+	if (!$query) {
+		return;
+	}
+
+	&sendres($from, $to, $id, $query);
+}
+
+sub sendtextres {
+	my($from, $to, $id, $query) = @_;
+
+	my $qres = `4s-query $OPT '$KB' '$query'`;
+	$qres =~ s/&/\&amp;/g;
+	$qres =~ s/</\&lt;/g;
+	$qres =~ s/>/\&gt;/g;
+$res = <<EOB;
+<message from='$from' id='$id' to='$to'>
+<body>
+$qres
+</body>
+</message>
+EOB
+	$Con->Send($res);
+}
+
+sub sendres {
+	my($from, $to, $id, $query) = @_;
+
+	my $qres = `4s-query $OPT '$KB' '$query'`;
+$res = <<EOB;
+<iq from='$from' id='$id' to='$to' type='result'>
+<query-result xmlns='http://www.w3.org/2005/09/xmpp-sparql-binding'>
+<meta comments='content supplied programmatically'/>
+$qres
+</query-result>
+</iq>
+EOB
+	$Con->Send($res);
+}
